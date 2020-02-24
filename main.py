78b47e721c9b006de80097f923bc9cae2f4e39af
y-shoji <p108053y@mail.kyutech.jp>
2020-02-24 17:37:05 +0900

    鉛筆画風変換実装

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import cv2
 
 from pathlib import Path
 from flask import Flask, request, abort
@@ -9,6 +10,7 @@ from linebot.models import (
     MessageEvent, TextMessage, ImageMessage, TextSendMessage, ImageSendMessage, 
     VideoSendMessage, StickerSendMessage, AudioSendMessage
 )
+from image_processing import *
 
 app = Flask(__name__)
 
@@ -20,6 +22,8 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 SRC_IMAGE_PATH = "static/images/{}.jpg"
+MAIN_IMAGE_PATH = "static/images/{}_main.jpg"
+PREVIEW_IMAGE_PATH = "static/images/{}_preview.jpg"
 
 def save_image(message_id: str, save_path: str) -> None:
     message_content = line_bot_api.get_message_content(message_id)
@@ -56,15 +60,42 @@ def handle_message(event):
 def handle_image(event):
     message_id = event.message.id
     src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
-
+    main_image_path = MAIN_IMAGE_PATH.format(message_id)
+    preview_image_path = PREVIEW_IMAGE_PATH.format(message_id)
+    
     save_image(message_id, src_image_path)
 
-    image_message = ImageSendMessage(
-        original_content_url=f"https://rinebot114514.herokuapp.com/{src_image_path}",
-        preview_image_url=f"https://rinebot114514.herokuapp.com/{src_image_path}",
-    )
-    line_bot_api.reply_message(event.reply_token, image_message)
+    img = cv2.imread(str(src_image_path),0)
     
+    hatching45_img, hatching135_img = hatching(img, LIY=30)
+    img2 = BD(hatching45_img, hatching135_img)
+    
+    max_size = 224
+    h,w =img2.shape[:2]
+
+    if w <= h and h > max_size:
+        proportion = h/max_size
+        pre_img = cv2.resize(img2, (int(w*proportion)-1, int(h*proportion)-1))
+    elif w > h and w > max_size:
+        proportion = w/max_size
+        pre_img = cv2.resize(img2, (int(w*proportion)-1, int(h*proportion)-1))
+    
+    app.logger.info(img2.shape,pre_img.shape)
+
+    cv2.imwrite(str(main_image_path),img2)
+    cv2.imwrite(str(preview_image_path),pre_img)
+
+    app.logger.info(os.path.isfile(f"https://date-the-image.herokuapp.com/{main_image_path}"))
+    app.logger.info(os.path.isfile(f"https://date-the-image.herokuapp.com/{preview_image_path}"))
+
+    image_message = ImageSendMessage(
+        original_content_url=f"https://rinebot114514.herokuapp.com/{main_image_path}",
+        preview_image_url=f"https://rinebot114514.herokuapp.com/{preview_image_path}",
+    )
+
+    line_bot_api.reply_message(event.reply_token, image_message)
+
+    src_image_path.unlink()
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
