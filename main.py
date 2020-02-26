6a114ef102031f73155cf622251ae2c787071f5b
Futaba-Kosuke <k.futaba2828@gmail.com>
2020-02-27 07:32:21 +0900

    Update: 1枚の画像から複数の画像への変換に対応

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-SRC_IMAGE_PATH = "static/images/{}.jpg"
-MAIN_IMAGE_PATH = "static/images/{}_main.jpg"
-PREVIEW_IMAGE_PATH = "static/images/{}_preview.jpg"
+SRC_IMAGE_PATH = "static/images/src/{}.jpg"
+MAIN_IMAGE_PATH = "static/images/{}/{}_main.jpg"
+PREVIEW_IMAGE_PATH = "static/images/{}/{}_preview.jpg"
 message_id = ""
 
 def save_image(message_id, save_path):
@@ -77,31 +77,31 @@ def resize(img, max_size):
     
     return img
 
-def image_converter(reply_token, mode=0):
+def image_converter(reply_token, mode):
     """
     0: cross_hatching
     1: tokyo_ghoul
+    2: mosaic
+    3: poster
     """
     global message_id
     src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
-    main_image_path = MAIN_IMAGE_PATH.format(message_id)
-    preview_image_path = PREVIEW_IMAGE_PATH.format(message_id)
+    main_image_path = MAIN_IMAGE_PATH.format(mode, message_id)
+    preview_image_path = PREVIEW_IMAGE_PATH.format(mode, message_id)
 
     img = cv2.imread(str(src_image_path))
 
-    print(img.shape)
     img = resize(img=img, max_size=1024)
-    print(img.shape)
 
-    if mode == 0:
+    if mode == 'cross_hatching':
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         hatching45_img, hatching135_img = hatching(img, LIY=30)
         img2 = BD(hatching45_img, hatching135_img)
-    elif mode == 1:
+    elif mode == 'tokyo_ghoul':
         img2 = ghoul_api.stylzie(img)
-    elif mode == 2:
+    elif mode == 'mosaic':
         img2 = mosaic_api.stylzie(img)
-    elif mode == 3:
+    elif mode == 'poster':
         img2 = Posterization_transfer(img)
 
     pre_img = resize(img=img2, max_size=224)
@@ -109,22 +109,22 @@ def image_converter(reply_token, mode=0):
     cv2.imwrite(str(main_image_path),img2)
     cv2.imwrite(str(preview_image_path),pre_img)
 
-    # image_message = ImageSendMessage(
-    #     original_content_url=f"https://ad8e83cc.ngrok.io/{main_image_path}",
-    #     preview_image_url=f"https://ad8e83cc.ngrok.io/{preview_image_path}",
-    # )
-
-    aws_save_image(str(main_image_path))
-    aws_save_image(str(preview_image_path))
-
     image_message = ImageSendMessage(
-        original_content_url=aws_get_url(str(main_image_path)),
-        preview_image_url=aws_get_url(str(preview_image_path)),
+        original_content_url=f"https://ad8e83cc.ngrok.io/{main_image_path}",
+        preview_image_url=f"https://ad8e83cc.ngrok.io/{preview_image_path}",
     )
 
-    line_bot_api.reply_message(reply_token, image_message)
+    # aws_save_image(str(main_image_path))
+    # aws_save_image(str(preview_image_path))
 
-    src_image_path.unlink()
+    # image_message = ImageSendMessage(
+    #     original_content_url=aws_get_url(str(main_image_path)),
+    #     preview_image_url=aws_get_url(str(preview_image_path)),
+    # )
+
+    line_bot_api.reply_message(reply_token, [image_message, make_button()])
+
+    # src_image_path.unlink()
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -146,18 +146,22 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
+    print(event.message.text)
     if message == "クロスハッチング":
-        image_converter(event.reply_token, 0)
+        image_converter(event.reply_token, 'cross_hatching')
     elif message == "東京喰種風":
-        image_converter(event.reply_token, 1)
+        image_converter(event.reply_token, 'tokyo_ghoul')
     elif message == "ステンドグラス風":
-        image_converter(event.reply_token, 2)
+        image_converter(event.reply_token, 'mosaic')
     elif message == "ポスター風":
-        image_converter(event.reply_token, 3)
+        image_converter(event.reply_token, 'poster')
     else:
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=f"{message}の処理が出来ませんでした。")
+            [
+                TextSendMessage(text=f"{message}の処理が出来ませんでした。"),
+                make_button()
+            ]
         )
 
 @handler.add(MessageEvent, message=ImageMessage)
