4e94c646c943f6c1ec13a8f4dcdea23aa2ca92bf
Mutsuki-Tashiro <mutsuki.tashiro@gmail.com>
2020-02-26 23:50:22 +0900

    update ボタンによる画像処理の分岐機能追加

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ handler = WebhookHandler(LINE_CHANNEL_SECRET)
 SRC_IMAGE_PATH = "static/images/{}.jpg"
 MAIN_IMAGE_PATH = "static/images/{}_main.jpg"
 PREVIEW_IMAGE_PATH = "static/images/{}_preview.jpg"
+message_id = ""
 
 def save_image(message_id, save_path):
     message_content = line_bot_api.get_message_content(message_id)
@@ -49,6 +50,39 @@ def make_button():
     )
     return message_template
 
+def cross_hatching(reply_token):
+    global message_id
+    src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
+    main_image_path = MAIN_IMAGE_PATH.format(message_id)
+    preview_image_path = PREVIEW_IMAGE_PATH.format(message_id)
+
+    img = cv2.imread(str(src_image_path),0)
+
+    hatching45_img, hatching135_img = hatching(img, LIY=30)
+    img2 = BD(hatching45_img, hatching135_img)
+            
+    max_size = 224
+    h,w =img2.shape[:2]
+
+    if w <= h and h > max_size:
+        proportion = h/max_size
+        pre_img = cv2.resize(img2, (int(w*proportion)-1, int(h*proportion)-1))
+    elif w > h and w > max_size:
+        proportion = w/max_size
+        pre_img = cv2.resize(img2, (int(w*proportion)-1, int(h*proportion)-1))
+
+    cv2.imwrite(str(main_image_path),img2)
+    cv2.imwrite(str(preview_image_path),pre_img)
+
+    image_message = ImageSendMessage(
+        original_content_url=f"https://2b0bcad0.ngrok.io/{main_image_path}",   #直前の画像
+        preview_image_url=f"https://2b0bcad0.ngrok.io/{preview_image_path}",
+    )
+
+    line_bot_api.reply_message(reply_token, image_message)
+
+    src_image_path.unlink()
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -69,50 +103,32 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=message))
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=message))
+    if message == "text1":
+        cross_hatching(event.reply_token)
+    elif message == "text2":
+        cross_hatching(event.reply_token)
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f"{message}の処理が出来ませんでした。")
+        )
+
+
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
+    global message_id
     message_id = event.message.id
     src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
-    main_image_path = MAIN_IMAGE_PATH.format(message_id)
-    preview_image_path = PREVIEW_IMAGE_PATH.format(message_id)
+    
     
     save_image(message_id, src_image_path)
-
-    img = cv2.imread(str(src_image_path),0)
-
-    hatching45_img, hatching135_img = hatching(img, LIY=30)
-    img2 = BD(hatching45_img, hatching135_img)
-    
-    max_size = 224
-    h,w =img2.shape[:2]
-
-    if w <= h and h > max_size:
-        proportion = h/max_size
-        pre_img = cv2.resize(img2, (int(w*proportion)-1, int(h*proportion)-1))
-    elif w > h and w > max_size:
-        proportion = w/max_size
-        pre_img = cv2.resize(img2, (int(w*proportion)-1, int(h*proportion)-1))
-    
-    # app.logger.info(img2.shape,pre_img.shape)
-
-    cv2.imwrite(str(main_image_path),img2)
-    cv2.imwrite(str(preview_image_path),pre_img)
-
-    # app.logger.info(os.path.isfile(f"https://date-the-image.herokuapp.com/{main_image_path}"))
-    # app.logger.info(os.path.isfile(f"https://date-the-image.herokuapp.com/{preview_image_path}"))
-
-    image_message = ImageSendMessage(
-        original_content_url=f"https://2b0bcad0.ngrok.io/{main_image_path}",
-        preview_image_url=f"https://2b0bcad0.ngrok.io/{preview_image_path}",
-    )
-
     line_bot_api.reply_message(event.reply_token, make_button())
 
-    src_image_path.unlink()
+
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
